a2003548a288885a3b5593578e9d29f81a839334
rulossan <165792347+rulossan@users.noreply.github.com>
2024-08-20 17:29:24 -0500

    main1

---
+++ src/javanotes/Operator.java
@@ -0,0 +1,18 @@
+package javanotes;
+
+public class Operator {
+    public static void main(String[] args) {
+
+        // Aritmeticos(+-*/,%)
+
+        int num1 = 50;
+        int num2 = 35;
+        int sum = num1 + num2;
+        System.out.println(sum);
+
+        int rest = num1 - num2;
+        System.out.println(rest);
+
+
+    }
+}
